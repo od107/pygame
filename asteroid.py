a376e3aa8c85589a0a3020e381e025a7e1a9ef7d
od107 <od107@users.noreply.github.com>
2019-02-07 17:46:22 +0100

    added bullets and fixed some bugs

--- asteroid.py
+++ asteroid.py
@@ -9,6 +9,7 @@ WINDOWWIDTH = 640 # size of window's width in pixels
 WINDOWHEIGHT = 480 # size of windows' height in pixels
 SHIPSIZE = 20
 MARGIN = 50
+BULLETSIZE = 3
 
 #            R    G    B
 GRAY     = (100, 100, 100)
@@ -46,18 +47,17 @@ def main():
     pygame.display.set_caption('Asteroids')
 
     #initialize
-
     ship = Ship()
-    #TODO: asteroids
-    
+    bullets = []
+    asteroids = []
 
     DISPLAYSURF.fill(BGCOLOR)
-    
+
     while True: # main game loop
         DISPLAYSURF.fill(BGCOLOR)
-        draw(ship)
+        draw(ship, bullets, asteroids)
 
-        #difference keyup and keypressed
+        #TODO: change keyup with keypressed
 
         for event in pygame.event.get(): # event handling loop
             if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
@@ -67,39 +67,53 @@ def main():
                 if event.key == K_UP:
                     ship.accelerate()
                 if event.key == K_SPACE:
-                    ship.fire()
+                    bullets.append(ship.shoot())
                 if event.key == K_RIGHT:
                     ship.rotateRight()
                 if event.key == K_LEFT:
                     ship.rotateLeft()
-    
+
         # Update position
         ship.move()
 
+        for asteroid in asteroids:
+            asteroid.move()
+
+        for bullet in bullets:
+            if bullet.move():
+                bullets.remove(bullet)
+
         pygame.display.update()
         FPSCLOCK.tick(FPS)
+        #print(FPSCLOCK.get_fps())
 
 
 
-def draw(ship):
-    #TODO fix border transitions
+def draw(ship, bullets, asteroids):
+    #TODO fix border transitions and add asteroids
+    #TODO change the position of the ship image to be at the tip of the ship
 
-    blittedRect = DISPLAYSURF.blit(ship.img, ship.pos)
+
+    blittedRect = DISPLAYSURF.blit(ship.img.convert(), ship.pos)
 
     oldCenter = blittedRect.center
     rotatedSurf = pygame.transform.rotate(ship.img, math.degrees(ship.orientation - math.pi/2))
 
     rotRect = rotatedSurf.get_rect()
     rotRect.center = oldCenter
-    
+
     DISPLAYSURF.blit(rotatedSurf, rotRect)
+
+    for bullet in bullets: #is there a way to blit multiple images?
+        #DISPLAYSURF.blit(bullet.img, bullet.pos)
+        pygame.draw.rect(DISPLAYSURF, WHITE, ((bullet.pos), (BULLETSIZE, BULLETSIZE)))
 
 
 class Ship:
     maxSpeed = 100
     img = pygame.image.load('ship.png')
     img.set_colorkey((0, 0, 0))
-    
+
     def __init__(self):
         self.pos = [int(WINDOWWIDTH/2), int(WINDOWHEIGHT/2)]
         self.orientation = math.pi/2
@@ -109,38 +123,58 @@ class Ship:
     def accelerate(self):
         self.vel[0] += math.cos(self.orientation)/5
         self.vel[1] += math.sin(self.orientation)/5
-        print('velocity: ',self.vel)
+        #print('velocity: ',self.vel)
         #TODO limit to max vel
 
     def move(self):
         self.pos[0] += self.vel[0]
         self.pos[1] -= self.vel[1]
-        #print('position: ',self.pos)
-           
-        if(self.pos[0] > WINDOWWIDTH + MARGIN):
+
+        #pos is the topleft most position of the ship image
+        if(self.pos[0] > WINDOWWIDTH + MARGIN + SHIPSIZE):
             self.pos[0] = 0 - MARGIN
-        elif(self.pos[1] > WINDOWHEIGHT + MARGIN):
+        elif(self.pos[1] > WINDOWHEIGHT + MARGIN + SHIPSIZE):
             self.pos[1] = 0 - MARGIN
         elif(self.pos[0] < 0 - MARGIN):
             self.pos[0] = WINDOWWIDTH + MARGIN
         elif(self.pos[1] < 0 - MARGIN):
-            self.pos[1] = WINDOWWIDTH + MARGIN
-        
+            self.pos[1] = WINDOWHEIGHT + MARGIN
+
 
     def rotateRight(self):
         self.orientation -= math.pi / 18
-        print('orientation: ', self.orientation)
+        #print('orientation: ', self.orientation)
 
     def rotateLeft(self):
         self.orientation += math.pi / 18
-        print('orientation: ', self.orientation)
+        #print('orientation: ', self.orientation)
 
     def shoot(self):
-        pass
+        bullet = Bullet(self.pos, self.orientation, self.vel)
+        return bullet
+        print('position: ', self.pos)
         #to implement
-        
 
-    
+class Bullet():
+    shootSpeed = 5
+
+    def __init__(self, shippos, shiporientation, shipvel):
+        self.pos = shippos.copy()
+        self.vel = [shipvel[0] + self.shootSpeed * math.cos(shiporientation),
+                    shipvel[1] + self.shootSpeed * math.sin(shiporientation)]
+
+    def move(self):
+        self.pos[0] += self.vel[0]
+        self.pos[1] -= self.vel[1]
+
+        #pos is the topleft most position of the ship image
+        if(self.pos[0] > WINDOWWIDTH + MARGIN + SHIPSIZE or
+            self.pos[1] > WINDOWHEIGHT + MARGIN + SHIPSIZE or
+            self.pos[0] < 0 - MARGIN or
+            self.pos[1] < 0 - MARGIN):
+            del(self)
+            return True
+
 
 
 if __name__ == '__main__':
